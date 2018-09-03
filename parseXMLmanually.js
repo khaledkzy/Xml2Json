ae24e12b2215f38e6ee9f87a64f533fa1001cd7f
khaledkzy <khaledkzy@gmail.com>
2018-09-03 08:01:39 +0100

    parser splits the XML file

--- parseXMLmanually.js
+++ parseXMLmanually.js
@@ -19,7 +19,50 @@ async function getXmlmanually() {
 
 const xml = "<a><b>crap</b></a>"
 
-console.log(xml.indexOf('>'))
+function xmlToJson(xml) {
+    if(calls > 10000) {
+      return;
+    }
+    if(!xml) {
+      return [];
+    }
+    let indexToStartAt = xml.search(/<[A-Z]/);
+    let hasNext = true;
+    const elements = [];
+    while(hasNext && calls < 10000) {
+      calls+=1;
+      const firstPar = xml.indexOf('<', indexToStartAt);
+      const firstSpace = xml.indexOf(' ', firstPar);
+      const matchingPar = xml.indexOf('>', firstPar);
+
+      const elementEndsAt = firstSpace > -1 ? firstSpace : matchingPar;
+      const tagName = xml.substring(firstPar + 1, elementEndsAt);
+
+      const closingTag = `</${tagName}>`; // </A>
+      const startClosingIndex = xml.indexOf(closingTag);
+      const children = xml.substring(matchingPar + 1, startClosingIndex)
+      .trim()
+      .replace(' ', '')
+      .replace('\n','')
+      .replace(' ','');
+     
+      const parsedChildren = xmlToJson(children);
+
+      indexToStartAt = startClosingIndex + closingTag.length;
+      hasNext =  xml.indexOf('<', indexToStartAt) > -1;
+      elements.push({
+        tagName,
+        children: parsedChildren,
+        attributes: []
+      });
+    }
+
+
+    // console.log(elements);
+    return elements;
+  }
+
+  // xmlToJson(test);
 
 
 
