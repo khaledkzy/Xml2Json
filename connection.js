710b85993a233714cb74f7f8d52ddaaec0b04988
khaledkzy <khaledkzy@gmail.com>
2018-09-04 17:21:41 +0100

    update DB name

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/cfDB");
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/financialclouds");
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
